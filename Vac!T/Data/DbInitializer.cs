fe89a45056df5b3982fe562f3f9a1895077ed7df
Daan Braas <daanbraas@gmail.com>
2023-08-31 17:06:15 +0200

    Data seeding

---
+++ Vac!T/Data/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Vac_T.Areas.Identity.Data;
+using Vac_T.Contexts;
+
+namespace Vac_T.Data
+{
+    public static class DbInitializer
+    {
+        public static void DbInitialize(VacItContext context) {
+            SeedEmployers(context);
+        }
+
+        private static void SeedEmployers(VacItContext context)
+        {
+
+            var employers = new List<Vac_TUser>()
+            {
+                new Vac_TUser(),
+                new Vac_TUser(),
+                new Vac_TUser()
+            };
+
+            foreach(Vac_TUser employer in employers)
+            {
+
+            }
+        }
+    }
+}
